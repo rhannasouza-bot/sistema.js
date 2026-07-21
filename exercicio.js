2e9d57f3cbb93e50293abbb7f051b49ea53fc08f
RHANNA RAQUEL NUNES DE SOUZA <rhanna_souza@estudante.sesisenai.org.br>
2026-07-21 16:47:12 -0300

    BuscarAluno, Situação, Media, Cadastrar, removerAlunos, Boletim, Totaldealunos, Medidageral,cadastro

--- exercicio.js
+++ exercicio.js
@@ -168,3 +168,190 @@ listarAlunos();
 lancarNota();
 
 verBoletim();
+
+function totalDeAlunos() {
+    console.log("Total de alunos: " + alunos.length);
+}
+
+function mediaGeralDaTurma() {
+    if (alunos.length === 0) {
+        console.log("Não há alunos cadastrados!");
+        return;
+    }
+
+    let soma = 0;
+
+    for (let i = 0; i < alunos.length; i++) {
+        soma += calcularMedia(alunos[i]);
+    }
+
+    let mediaGeral = soma / alunos.length;
+
+    console.log("Média geral da turma: " + mediaGeral.toFixed(2));
+}
+
+function listarAprovados() {
+    let texto = "Alunos aprovados:\n";
+    let encontrou = false;
+
+    for (let i = 0; i < alunos.length; i++) {
+        let media = calcularMedia(alunos[i]);
+
+        if (situacao(media) === "APROVADO") {
+            texto += alunos[i].nome + " - Média: " + media.toFixed(2) + "\n";
+            encontrou = true;
+        }
+    }
+
+    if (encontrou) {
+        console.log(texto);
+    } else {
+        console.log("Ainda não há alunos aprovados.");
+    }
+}
+
+totalDeAlunos();
+mediaGeralDaTurma();
+listarAprovados();
+
+function submenuCadastro() {
+    let opcao;
+
+    do {
+        opcao = Number(prompt(
+            "\n=== MENU CADASTRO ===\n" +
+            "1 - Cadastrar aluno\n" +
+            "2 - Listar alunos\n" +
+            "3 - Remover aluno\n" +
+            "0 - Voltar\n" +
+            "Escolha uma opção: "
+        ));
+
+        switch (opcao) {
+            case 1:
+                cadastrarAluno();
+                break;
+
+            case 2:
+                listarAlunos();
+                break;
+
+            case 3:
+                removerAluno();
+                break;
+
+            case 0:
+                break;
+
+            default:
+                console.log("Opção inválida!");
+        }
+
+    } while (opcao !== 0);
+}
+
+function submenuNotas() {
+    let opcao;
+
+    do {
+        opcao = Number(prompt(
+            "\n=== MENU NOTAS ===\n" +
+            "1 - Lançar nota\n" +
+            "2 - Ver boletim do aluno\n" +
+            "0 - Voltar\n" +
+            "Escolha uma opção: "
+        ));
+
+        switch (opcao) {
+            case 1:
+                lancarNota();
+                break;
+
+            case 2:
+                verBoletim();
+                break;
+
+            case 0:
+                break;
+
+            default:
+                console.log("Opção inválida!");
+        }
+
+    } while (opcao !== 0);
+}
+
+function submenuRelatorios() {
+    let opcao;
+
+    do {
+        opcao = Number(prompt(
+            "\n=== MENU RELATÓRIOS ===\n" +
+            "1 - Total de alunos\n" +
+            "2 - Média geral da turma\n" +
+            "3 - Listar aprovados\n" +
+            "0 - Voltar\n" +
+            "Escolha uma opção: "
+        ));
+
+        switch (opcao) {
+            case 1:
+                totalDeAlunos();
+                break;
+
+            case 2:
+                mediaGeralDaTurma();
+                break;
+
+            case 3:
+                listarAprovados();
+                break;
+
+            case 0:
+                break;
+
+            default:
+                console.log("Opção inválida!");
+        }
+
+    } while (opcao !== 0);
+}
+
+function menuPrincipal() {
+    let opcao;
+
+    do {
+        opcao = Number(prompt(
+            "\n=== SISTEMA ESCOLAR ===\n" +
+            "1 - Cadastro\n" +
+            "2 - Notas\n" +
+            "3 - Relatórios\n" +
+            "0 - Sair\n" +
+            "Escolha uma opção: "
+        ));
+
+        switch (opcao) {
+            case 1:
+                submenuCadastro();
+                break;
+
+            case 2:
+                submenuNotas();
+                break;
+
+            case 3:
+                submenuRelatorios();
+                break;
+
+            case 0:
+                console.log("Encerrando o sistema...");
+                break;
+
+            default:
+                console.log("Opção inválida!");
+        }
+
+    } while (opcao !== 0);
+}
+
+menuPrincipal();
